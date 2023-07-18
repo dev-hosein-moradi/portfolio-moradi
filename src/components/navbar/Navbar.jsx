288161e60f2b66ddfc8393bfca95a81b4c0301d3
hoseinmoradi <moradih7379@gmail.com>
2023-07-18 12:12:34 +0330

    ➕ add framer motion animation to navbar items on scroll.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./navbar.css";
 import { useDispatch } from "react-redux";
 import { themeActions } from "../../redux/slices/themeSlice";
+import { motion } from "framer-motion";
 
 const Navbar = ({ scrollToTitle }) => {
   const dispatch = useDispatch();
@@ -15,10 +16,16 @@ const Navbar = ({ scrollToTitle }) => {
       {/* for define items */}
       <div className="navbar__links">
         <ul className="links__items">
-          {/* about me, projects, contact me */}
-          <li onClick={() => scrollToTitle("about")}>01 - About-Me</li>
-          <li onClick={() => scrollToTitle("project")}>02 - Projects</li>
-          <li onClick={() => scrollToTitle("contact")}>03 - Contact-Me</li>
+          <motion.div
+            initial={{ opacity: 0, x: "-100%" }}
+            transition={{ duration: 0.8 }}
+            whileInView={{ opacity: 1, x: 0 }}
+          >
+            {/* about me, projects, contact me */}
+            <li onClick={() => scrollToTitle("project")}>01 - Projects</li>
+            <li onClick={() => scrollToTitle("about")}>02 - About-Me</li>
+            <li onClick={() => scrollToTitle("contact")}>03 - Contact-Me</li>
+          </motion.div>
         </ul>
       </div>
 
